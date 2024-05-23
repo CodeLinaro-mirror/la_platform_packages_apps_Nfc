6e8b8d92bf4bbf403eb0f017df665a559ad0ff71
Alisher Alikhodjaev <alisher@google.com>
2024-05-23 11:35:12 +0530

    Subject: [PATCH] Ensure that SecureNFC setting cannot be bypassed
    Bug: 268038643
    Test: ctsverifier
    (cherry picked from commit 6cb53d963d376b97963120a4d2c7df961789e428)
    Merged-In: I53a45c3600dc6bba7009921ca5135ee37b5edfd0
    Change-Id: I53a45c3600dc6bba7009921ca5135ee37b5edfd0

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -1169,6 +1169,12 @@ public class NfcService implements DeviceHostListener {
         }
     }
 
+    public boolean isSecureNfcEnabled() {
+        synchronized (NfcService.this) {
+            return mIsSecureNfcEnabled;
+        }
+    }
+
     final class NfcAdapterService extends INfcAdapter.Stub {
         @Override
         public boolean enable() throws RemoteException {
